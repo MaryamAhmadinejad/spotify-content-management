06b016908ae3ef1f9fb41e192e6cc0b5f1f768cb
Ali Ramezani <127216595+RamezaniAli@users.noreply.github.com>
2025-02-25 03:28:59 +0330

    Update bronze_listen_events.sql

--- dbt_project/models/bronze/bronze_listen_events.sql
+++ dbt_project/models/bronze/bronze_listen_events.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized='table',
     file_format='parquet',
-    external_location='hdfs://namenode:9000/output/listen_events'
+    location_root='hdfs://namenode:9000/bronze/listen_events',
+    external=true
 ) }}
 
 select * from parquet.`hdfs://namenode:9000/output/listen_events`
