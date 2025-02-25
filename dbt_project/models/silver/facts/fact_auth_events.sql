f2f60504b0d568c1fd098f0e8831c0ea52cb1b68
Ali Ramezani <127216595+RamezaniAli@users.noreply.github.com>
2025-02-25 03:30:55 +0330

    Update fact_auth_events.sql

--- dbt_project/models/silver/facts/fact_auth_events.sql
+++ dbt_project/models/silver/facts/fact_auth_events.sql
@@ -1,4 +1,10 @@
-{{ config(materialized='table', file_format='parquet', partition_by=['year', 'month', 'day']) }}
+{{ config(
+    materialized='table',
+    file_format='parquet',
+    location_root='hdfs://namenode:9000/silver/fact_auth_events',
+    external=true,
+    partition_by=['year', 'month', 'day']
+) }}
 
 select
   md5(concat(cast(userId as string), cast(ts as string))) as auth_event_id,
